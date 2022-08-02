956a93b60128f88b20dfa7782fa35cf5cd8aaf1d
Your Name <dat054760@gmail.com>
2022-08-03 03:26:10 +0700

    up

---
+++ src/rikkei/academy/FizzBuzzTest.java
@@ -0,0 +1,34 @@
+package rikkei.academy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzTest {
+    @Test
+    void testFizz3() {
+        int number = 3;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFizz6() {
+        int number = 6;
+        String expected = "Fizz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testFizz5() {
+        int number = 5;
+        String expected = "Buzz";
+
+        String result = FizzBuzz.fizzBuzz(number);
+        assertEquals(expected, result);
+    }
+}
